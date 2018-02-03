75dfe2bbf5fd1cca991fd614d13c30cb4b1d72f5
arjunpat <ajpat1234@gmail.com>
2018-02-03 11:26:45 -0800

    a

---
+++ web/js/global.js
@@ -0,0 +1,9 @@
+window.onscroll = function(e) {
+	if ($(document).scrollTop() === 0) {
+		$('.navbar').css('box-shadow', 'none');
+		$('.navbar').css('border-bottom', 'none');
+	} else {
+		$('.navbar').css('box-shadow', '0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28)');
+		$('.navbar').css('border-bottom', '3px solid red');
+	}
+};
